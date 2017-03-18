3173ea82dad119d15ac87a2a431ed293cbfc5efa
unknown <jasonpineda100@gmail.com>
2017-03-18 11:04:28 -0500

    login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,18 @@
 angular.module('app.controllers', [])
-  
-.controller('loginCtrl', function($scope) {
 
+.controller('loginCtrl', function($scope, $http) {
+  localStorage.clear();
+   $scope.data = {};
+    $scope.login = function() {
+      $http.post("http://vendelo-api.herokuapp.com/api/v1/login", 
+    {"auth":{"email":$scope.data.username,"password":$scope.data.password}}).then(function(resp) {       
+        $scope.token = resp.data['jwt'];
+        localStorage.setItem("token", $scope.token);
+      }, function(err) {
+        console.error('ERR', err);
+        // err.status will contain the status code
+      })
+      }
 })
    
 .controller('galeriaCtrl', function($scope, $stateParams, $http) {
@@ -22,13 +33,15 @@ angular.module('app.controllers', [])
    
 .controller('obrasCtrl', function($scope, $http) {
 	$scope.result = "";
-  $http.get('https://vendelo-api.herokuapp.com/api/v1/artworks')
-    .success(function(data, status, headers,config){
+  $http.get('https://vendelo-api.herokuapp.com/api/v1/artworks', {
+    headers: {'Authorization':'Bearer ' + localStorage.getItem("token")}
+}).success(function(data, status, headers,config){
       console.log('data success');
       console.log(data); // for browser console
       $scope.result = data; // for UI
     })
     .error(function(data, status, headers,config){
+      localStorage.clear();
       console.log('data error');
     })
     .then(function(result){
